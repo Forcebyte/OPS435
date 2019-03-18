5349f97aef052b56bd4d61c88ce84589c2967cef
Patrick <pturney@myseneca.ca>
2019-03-17 23:26:01 -0400

    Update ur_pturney.py

--- ur_pturney.py
+++ ur_pturney.py
@@ -282,8 +282,8 @@ def cal_weekly_usage(login_recs,args):
 		enddate_wk = str(strftime("%U", enddate_obj))
 
 		#Since week 1 is 0, we add 1 to the weeknumber to get our actual week
-		startdate_wk =str(int(startdate_wk) + 1)
-		enddate_wk = str(int(enddate_wk) + 1)
+		startdate_wk = str(int(startdate_wk))
+		enddate_wk = str(int(enddate_wk))
 
 		#generates a date object to use in the dictionary we will comapre to
 		#dictmonth = {"Jan":"1", "Feb":"2","Mar":"3","Apr":"4","May":"5","Jun":"6","Jul":"7","Aug":"8","Sep":"9","Oct":"10","Nov":"11","Dec":"12"}
@@ -291,10 +291,12 @@ def cal_weekly_usage(login_recs,args):
 		#If the entry is not on the same week, we take the median date between the two
 		#After taking the median day, startdate usage is equal to the median day minus the startdate
 		#Likewise, the ending day is just equal to the ending time...
+		print(startdate_wk)
+		print(enddate_wk)
 		if startdate_wk != enddate_wk:
 			modday = enddate_str[0:6] + " " + enddate_str[16::]
 			print(modday)
-			nextday = time.strptime(modday, '%d %Y')
+			nextday = time.strptime(modday, '%b %d %Y')
 
 			#Previous day (12:00 - XX:XX) time difference, take it and round it out
 			prvday_timediff = (time.mktime(nextday) - time.mktime(startdate_obj))
